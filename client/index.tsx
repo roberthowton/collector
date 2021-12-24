7665bd01cfa4555429f1ceb5a0a0a93e20bbc5ed
Robert Howton <robert.f.howton@gmail.com>
2021-12-24 14:23:41 -0500

    migrated to ts

--- client/index.tsx
+++ client/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import * as React from 'react';
 import { render } from 'react-dom';
 // import { BrowserRouter, Link } from 'react-router-dom';
 import App from './App';
 
 render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('app'),
 );
